347d42a1edf6315b3f4f72d88d7caf0846ed3b49
Digimonchik <da.logvinenko@gmail.com>
2022-02-13 22:47:03 +0300

    Update index.js

--- lesson1/task1/index.js
+++ lesson1/task1/index.js
@@ -1,11 +1,11 @@
-const rootElement = document.querySelector("#root");
+// const rootElement = document.querySelector("#root");
 
-const greatingElem = React.createElement(
-  "div",
-  {
-    className: "greeting",
-  },
-  "Hello, React"
-);
+// const greatingElem = React.createElement(
+//   "div",
+//   {
+//     className: "greeting",
+//   },
+//   "Hello, React"
+// );
 
-ReactDOM.render(greatingElem, rootElement);
+// ReactDOM.render(greatingElem, rootElement);
