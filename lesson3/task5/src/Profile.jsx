9b4a8e73cbff5e76bb37afeb38d7f2c2ab9c0382
Digimonchik <da.logvinenko@gmail.com>
2022-03-03 20:40:18 +0300

    update

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -4,9 +4,14 @@ import moment from 'moment';
 
 const Profile = (props) => {
     console.log(props.user)
-    return <div className='profile'>
-        {`${props.user.firstName} ${props.user.lastName} Was born ${moment(props.user.birthDate).format('DD MMM YY')} in ${props.user.birthPlace}`}
+    return <div className='container'>
+    <div className='profile__name'>
+        {`${props.user.firstName} ${props.user.lastName}`}
     </div>
+    <div className='profile__birth'>
+            {`Was born ${moment(props.user.birthDate).format('DD MMM YY')} in ${props.user.birthPlace}`}
+        </div>
+        </div>
 }
 
 export default Profile
