4e6c3b7b6b2de1c5302a59d9661d396a8c0289cc
Digimonchik <da.logvinenko@gmail.com>
2022-03-10 17:19:08 +0300

    update

--- lesson3/task4/src/Greeting.jsx
+++ lesson3/task4/src/Greeting.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Greeting = (props) => {
+function Greeting(props) {
+    
     const getAge = (date) => {
-    const timeOfbirth = new Date(date).getTime();
-    const curentTime = new Date();
-    return Math.floor((curentTime - timeOfbirth) / 31536000000); }
+        const timeOfbirth = new Date(date).getTime();
+        const curentTime = new Date();
+        return Math.floor((curentTime - timeOfbirth) / 31536000000);
+    }
 const element = <div className="greeting">
         {`My name is 
         ${props.firstName} 
