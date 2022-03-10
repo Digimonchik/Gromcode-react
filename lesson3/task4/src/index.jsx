63bb631e6f100c95e1b6e6218b2b2c985336b646
Digimonchik <da.logvinenko@gmail.com>
2022-03-10 17:24:58 +0300

    update

--- lesson3/task4/src/index.jsx
+++ lesson3/task4/src/index.jsx
@@ -5,11 +5,17 @@ import Greeting from "./Greeting";
 
 const rootElement = document.querySelector("#root");
 
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  birthDate: new Date('2005-01-01T11:11:11.819Z')
+}
+
 ReactDOM.render(
   <Greeting
-    firstName="John"
-    lastName="Doe"
-    birthDate={new Date('2005-01-01T11:11:11.819Z')}  
-  />,
+    firstName={user.firstName}
+    lastName={user.lastName}
+    birthDate={user.birthDate}  
+  />, 
   rootElement
 );
