f66afceb9f83520055cc533610a97902551f818f
Digimonchik <da.logvinenko@gmail.com>
2022-03-10 19:50:07 +0300

    update

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -5,10 +5,10 @@ import moment from 'moment';
 const Profile = (props) => {
     const element = (<>
     <div className='profile__name'>
-        {`${props.user.firstName} ${props.user.lastName}`}
+        {props.user.firstName} {props.user.lastName}
     </div>
     <div className='profile__birth'>
-            {`Was born ${moment(props.user.birthDate).format('DD MMM YY')} in ${props.user.birthPlace}`}
+        Was born {moment(props.user.birthDate).format('DD MMM YY')} in {props.user.birthPlace}
         </div>
     </>)
     return element
