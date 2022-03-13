26355c449f488475ed4c9b2d4be6361adc42e0aa
Digimonchik <da.logvinenko@gmail.com>
2022-03-13 18:57:03 +0300

    update

--- lesson4/task2/src/App.jsx
+++ lesson4/task2/src/App.jsx
@@ -4,12 +4,12 @@ import Clock from './Clock';
 const App = () => {
     return <>
         <Clock location="New York"
-            offset={-7}></Clock>
+            offset={-5}></Clock>
     <Clock location="Kiev"
             offset={2}></Clock>
         
         <Clock location="London"
-            offset={1}></Clock>
+            offset={0}></Clock>
         </>
 }
 
