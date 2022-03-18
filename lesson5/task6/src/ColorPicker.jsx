03bdd42beaeb8412bfb7bc42901bfa8c918097ec
Digimonchik <da.logvinenko@gmail.com>
2022-03-18 23:23:07 +0300

    update

--- lesson5/task6/src/ColorPicker.jsx
+++ lesson5/task6/src/ColorPicker.jsx
@@ -16,7 +16,7 @@ export default class Toggler extends Component {
     const newValue = str[0].toUpperCase() + str.slice(1);
     this.setState({
       value: newValue
-  })
+    })
   }
   clearValue = () => {
         this.setState({
@@ -30,7 +30,7 @@ export default class Toggler extends Component {
     <div>
         <div className="picker__title">{this.state.value}</div>
                 <div>
-          <button className="picker__button picker__button_coral" style={{ backgroundColor: 'coral', }} onMouseEnter={(e) => this.changeColor(e)} onMouseLeave={() => this.clearValue()}/>
+          <button className="picker__button picker__button_coral" style={{backgroundColor: 'coral', }} onMouseEnter={(e) => this.changeColor(e)} onMouseLeave={() => this.clearValue()}/>
                 <button className="picker__button picker__button_aqua" style={{backgroundColor: 'aqua',}} onMouseEnter = {(e) => this.changeColor(e)} onMouseLeave={() => this.clearValue()}/>
                 <button className="picker__button picker__button_bisque" style={{backgroundColor: 'bisque',}} onMouseEnter = {(e) => this.changeColor(e)} onMouseLeave={() => this.clearValue()}/>
       </div>
