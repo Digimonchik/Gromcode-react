84d50d19ad8092cbee5eeb9124de67044e94a948
Digimonchik <da.logvinenko@gmail.com>
2022-03-22 17:54:40 +0300

    update

--- lesson6/task1/src/GuestGreeting.jsx
+++ lesson6/task1/src/GuestGreeting.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class GuestGreeting extends Component {
   render() {
     return (
-      <h1>Hello, stranger ✌️ Please login 🚀</h1>
+      <h1>Hello, stranger. Please login 🚀</h1>
     );
   }
 }
