3daed7b61552608c6c93779629b2d7a38e8af5c8
Digimonchik <da.logvinenko@gmail.com>
2022-03-25 17:56:13 +0300

    update

--- lesson6/task1/src/Logout.jsx
+++ lesson6/task1/src/Logout.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-export default class Logout2 extends Component {
+export default class Logout extends Component {
     constructor(props) {
 
         super(props)
   }
-    
   render() {
+
     return (
       <button className='btn logout' onClick={() => this.props.onLogout()}>Logout</button>
     );
