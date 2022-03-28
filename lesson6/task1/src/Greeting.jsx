5d169abefffaa8f0dd2b6a6a6488edbd033a4d29
Digimonchik <da.logvinenko@gmail.com>
2022-03-28 13:11:40 +0300

    update

--- lesson6/task1/src/Greeting.jsx
+++ lesson6/task1/src/Greeting.jsx
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import UserGreeting from './UserGreeting';
 import GuestGreeting from './GuestGreeting';
 
-export default class Greeting extends Component {
-    constructor(props) {
-        super(props)
-    }
-  render() {
-    return (
-                <div>
-                    {this.props.isLoggedIn ? <UserGreeting/> : <GuestGreeting/>}
-                </div>
-                
-    );
+const Greeting = props => {
+  if (props.isLoggedIn) {
+    return <UserGreeting/>
   }
+  return <GuestGreeting/>
 }
+
+export default Greeting;
+
+
