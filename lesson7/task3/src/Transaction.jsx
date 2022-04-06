a7232490e8e968abbf227595c317d399eee49113
Digimonchik <da.logvinenko@gmail.com>
2022-04-07 01:44:09 +0300

    update

--- lesson7/task3/src/Transaction.jsx
+++ lesson7/task3/src/Transaction.jsx
@@ -8,7 +8,7 @@ export default class Transaction extends Component {
       <li className= 'transaction'>
     <span className="transaction__date">{moment(this.props.time).format("DD MMM")}</span>
     <span className="transaction__time">{moment(this.props.time).format("HH:mm")}</span>
-    <span className="transaction__assets">{`${this.props.from}→${this.props.to}`}</span>
+    <span className="transaction__assets">{`${this.props.from} → ${this.props.to}`}</span>
     <span className="transaction__rate">{this.props.rate}</span>
     <span className="transaction__amount">{new Intl.NumberFormat('en-GB').format(this.props.amount)}</span>
       </li> 
