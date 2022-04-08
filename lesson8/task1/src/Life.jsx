a84a1fbd69d0a30170c5d91e390c0a139cee462b
Digimonchik <da.logvinenko@gmail.com>
2022-04-08 18:46:07 +0300

    update

---
+++ lesson8/task1/src/Life.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class Life extends Component {
+    constructor(props) {
+        super(props)
+        console.log(`constructor: good place to create state`)
+    }
+componentDidMount(){
+    console.log(`componentDidMount: API calls`)
+}
+
+shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render')
+}
+componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
+}
+componentWillUnmount() {
+    console.log(`cleanup before DOM related to component will be removed`)
+}
+
+  render() {
+    return (
+      <div > textInComponent </div>
+    );
+  }
+}
---
+++ lesson8/task1/src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.scss';
+import Life from './Life'
+
+const rootElement = document.querySelector('#root')
+
+ReactDOM.render(
+    <Life/>, rootElement
+)
