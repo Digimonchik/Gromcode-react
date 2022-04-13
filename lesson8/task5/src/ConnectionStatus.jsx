27aeee0848339cfc1abd9c4641e4fdf96a685439
Digimonchik <da.logvinenko@gmail.com>
2022-04-14 00:26:17 +0300

    update

--- lesson8/task5/src/ConnectionStatus.jsx
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -6,7 +6,6 @@ export default class ConnectionStatus extends Component {
   }
 
   setStatus  = () => {
-      console.log(this.state)
     this.setState(
         {
             OnlineStatus: !this.state.OnlineStatus
@@ -27,7 +26,7 @@ export default class ConnectionStatus extends Component {
 
     render() {
     return (
-      this.state.OnlineStatus ? <div className = 'status'>online</div> : <div className = 'status status status_offline'>offline</div>
+      this.state.OnlineStatus ? <div className = 'status'>online</div> : <div className = 'status status status_offline'>online</div>
       
     );
   }
