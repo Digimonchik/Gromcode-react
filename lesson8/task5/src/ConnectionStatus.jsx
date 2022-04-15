28345170c63e366d4a5802110e6cb4fd584d02a9
Digimonchik <da.logvinenko@gmail.com>
2022-04-16 01:00:34 +0300

    update

--- lesson8/task5/src/ConnectionStatus.jsx
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -2,32 +2,38 @@ import React, { Component } from 'react';
 
 export default class ConnectionStatus extends Component {
   state = {
-      status: true
+      status: 'online'
   }
 
-  setStatus  = () => {
+  setOnline = () => {
     this.setState(
         {
-            status: !this.state.status
+            status: 'online'
         }
-    )
-  }
+    )}
+    setOffline = () => {
+      this.setState(
+          {
+              status: 'offline'
+          }
+      )}
+    
 
 
   componentDidMount() {
-      window.addEventListener('online', this.setStatus),
-      window.addEventListener('offline', this.setStatus)
+      window.addEventListener('online', this.setOnline),
+      window.addEventListener('offline', this.setOffline)
       }
 
     componentWillUnmount() {
-      window.removeEventListener('online', this.setStatus),
-      window.removeEventListener('offline', this.setStatus)
+      window.removeEventListener('online', this.setOnline),
+      window.removeEventListener('offline', this.setOffline)
     }
 
 
     render() {
     return (
-      this.state.status === true ? <div className = 'status'>online</div> : <div className = 'status status_offline'>offline</div>
+      this.state.status === 'online' ? <div className = 'status'>online</div> : <div className = 'status status_offline'>offline</div>
       
     );
   }
