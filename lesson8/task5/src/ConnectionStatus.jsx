ca3deba6a2ca403c6e0c5457e287b0223c998d98
Digimonchik <da.logvinenko@gmail.com>
2022-04-15 21:09:56 +0300

    update

--- lesson8/task5/src/ConnectionStatus.jsx
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -12,20 +12,24 @@ export default class ConnectionStatus extends Component {
         }
     )
   }
+  componentDidUpdate() {
+    window.addEventListener('online', this.setStatus)
+        window.addEventListener('offline', this.setStatus)
+        
+  }
 
   componentDidMount() {
       window.addEventListener('online', this.setStatus)
         window.addEventListener('offline', this.setStatus)
         }
 
-    // componentWillUnmount() {
-    //   window.removeEventListener('online', this.setStatus)
-    //     window.removeEventListener('offline', this.setStatus)
-    // }
+    componentWillUnmount() {
+      window.removeEventListener('online', this.setStatus)
+        window.removeEventListener('offline', this.setStatus)
+    }
 
 
     render() {
-      console.log(this.state.OnlineStatus)
     return (
       this.state.OnlineStatus ? <div className = 'status'>online</div> : <div className = 'status status_offline'>offline</div>
       
