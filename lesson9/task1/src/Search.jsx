a3e5abd07e2a7ddf1cdc065242cdb2cc8739df2e
Digimonchik <da.logvinenko@gmail.com>
2022-04-17 00:06:40 +0300

    update

---
+++ lesson9/task1/src/Search.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+    state = {
+        text: ''
+    }
+    onInput = (event) => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+    handleClick = (event) => {
+        event.preventDefault()
+        alert(`Search text: ${this.state.text}`)
+    }
+  render() {
+    return (
+        <form className="search">
+        <input type="text" className="search__input" onChange = {this.onInput} value ={this.state.text}/>
+        <button className="search__button" onClick={this.handleClick}>Search</button>
+      </form>
+    );
+  }
+}
---
+++ lesson9/task1/src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.scss';
+import Search from './Search'
+
+const rootElement = document.querySelector('#root')
+
+ReactDOM.render(
+    <Search/>, rootElement
+)
