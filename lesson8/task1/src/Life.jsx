474aea869315295a275e04d661a5517e3f35514c
Digimonchik <da.logvinenko@gmail.com>
2022-04-16 16:11:21 +0300

    update task1

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Life extends Component {
     constructor(props) {
         super(props)
-        state = {
+        this.state = {
             status: 'working'
         }
         console.log(`constructor: good place to create state`)
@@ -12,12 +12,13 @@ componentDidMount(){
     console.log(`componentDidMount: API calls, subscriptions`)
 }
 
+componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
+}
 shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render')
 }
-componentDidUpdate() {
-    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
-}
+
 componentWillUnmount() {
     console.log(`componentWillUnmount(): cleanup before DOM related to component will be removed`)
 }
