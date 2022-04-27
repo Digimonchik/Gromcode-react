9f0d364f6f626fd30093ad1a25c6c8f15beec723
Digimonchik <da.logvinenko@gmail.com>
2022-04-27 23:16:54 +0300

    update

---
+++ lesson9/task3/src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.scss';
+import App from './UserForm';
+
+const rootElement = document.querySelector('#root')
+
+ReactDOM.render(
+    <App/>, rootElement
+)
