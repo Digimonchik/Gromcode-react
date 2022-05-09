05f070307bae673f4fddc976fe184674bf3ca908
Digimonchik <da.logvinenko@gmail.com>
2022-05-09 23:09:44 +0300

    update

--- lesson9/task1/src/Search.jsx
+++ lesson9/task1/src/Search.jsx
@@ -4,15 +4,18 @@ export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {value: ''};
-  }
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
   handleChange = (event) => {
         this.setState({
             value: event.target.value
         })
     }
     handleSubmit = (event) => {
-      alert('Search text: good restaurant');        
+      alert('Search text: ' + this.state.value);        
       event.preventDefault()
 
       }
