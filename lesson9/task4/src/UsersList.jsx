7bb6be955853be9ebde6f94aa4f7c1ca2df9d593
Digimonchik <da.logvinenko@gmail.com>
2022-05-17 22:44:19 +0300

    update

--- lesson9/task4/src/UsersList.jsx
+++ lesson9/task4/src/UsersList.jsx
@@ -2,11 +2,14 @@ import User from './User';
 import React, { Component } from 'react';
 
 export default class UsersList extends Component {
+  state = {
+    users: this.props.users
+  }
 
   render() {
     return (
     <ul className="users">
-    {this.props.users.map(element => <User name = {element.name} age = {element.age} key = {element.id}/>)}
+    {this.state.users.map(element => <User name = {element.name} age = {element.age} key = {element.id}/>)}
     </ul>
     );
   }
