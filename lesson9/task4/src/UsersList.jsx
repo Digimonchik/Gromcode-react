04d8dec145a7557b98803c762a56c83dd7caf500
Digimonchik <da.logvinenko@gmail.com>
2022-05-18 15:49:51 +0300

    update

--- lesson9/task4/src/UsersList.jsx
+++ lesson9/task4/src/UsersList.jsx
@@ -1,17 +1,33 @@
-import User from './User';
 import React, { Component } from 'react';
+import Filter from './Filter';
+import User from './User';
+
 
 export default class UsersList extends Component {
-  state = {
-    users: this.props.users
-  }
+    state = {
+        count: this.props.users.length,
+        filterText: '',
+        users: this.props.users
+    }
+
+    onChange = (event) => {
+        const filteredList =  this.props.users.filter(el => el.name.toLowerCase().includes(event.target.value.toLowerCase()))
+        this.setState(
+          {value: event.target.value,
+            count: filteredList.length,
+            users: filteredList
+        })
+      } 
+     
 
   render() {
     return (
-    <ul className="users">
+        <div>
+            <Filter onChange = {this.onChange} value = {this.state.filterText} count = {this.state.count}/>
+            <ul className="users">
     {this.state.users.map(element => <User name = {element.name} age = {element.age} key = {element.id}/>)}
     </ul>
+        </div>
     );
   }
 }
-
--- lesson9/task4/src/index.jsx
+++ lesson9/task4/src/index.jsx
@@ -1,12 +1,23 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.scss';
-import App from './App';
+import UsersList from './UsersList'
 
-
+const usersList = [
+    {
+        name: 'Dima',
+        age: 40,
+        id: 1,
+    },
+    {
+        name: 'Alex',
+        age: 26,
+        id: 2,
+    }
+]
 
 
 const rootElement = document.querySelector('#root') 
 ReactDOM.render(
-    <App users = {[]}/>, rootElement
+    <UsersList users = {[]}/>, rootElement
 )
