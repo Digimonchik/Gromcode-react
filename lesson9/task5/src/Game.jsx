65c68539d76e4ac41f86dcf17387aaac81c3ae01
Digimonchik <da.logvinenko@gmail.com>
2022-05-26 23:17:53 +0300

    update

--- lesson9/task5/src/Game.jsx
+++ lesson9/task5/src/Game.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import Board from "./Board";
-import calculateWinner from "./index.jsx";
-
 
 export default class Game extends Component {
   constructor(props) {
@@ -16,12 +14,31 @@ export default class Game extends Component {
       xIsNext: true
     };
   }
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    if (this.calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
@@ -46,7 +63,7 @@ export default class Game extends Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winner = this.calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ?
